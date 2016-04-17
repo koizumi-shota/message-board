19b191f53b46038bfae55d2109b934999d65aa2e
koizumi_shota <koizumishota1124@gmail.com>
2016-04-17 01:15:03 +0000

    masuter-challege2-３ heroku

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,6 +4,6 @@ class Message < ActiveRecord::Base
   # 内容は必須入力かつ2文字以上30文字以下
   validates :body , length: { minimum: 2, maximum: 30 } , presence: true
   # 数値であり、0以上の場合有効
-  validates :age, numericality: { greater_than_or_equal_to: 0 } , presence: true
+  validates :age, numericality: { greater_than_or_equal_to: 0 }
   
 end
